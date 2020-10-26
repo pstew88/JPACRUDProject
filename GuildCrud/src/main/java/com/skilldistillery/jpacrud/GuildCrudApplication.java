e86d3e95395d5f232ecf37751ca76db47b86d672
Pierce <pierce.steward@gmail.com>
2020-10-26 10:13:26 -0600

    added war file

--- GuildCrud/src/main/java/com/skilldistillery/jpacrud/GuildCrudApplication.java
+++ GuildCrud/src/main/java/com/skilldistillery/jpacrud/GuildCrudApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.jpacrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GuildCrudApplication {
+public class GuildCrudApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(GuildCrudApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(GuildCrudApplication.class, args);
